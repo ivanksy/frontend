65f64f3671c01828a5a4a3b04c0f2ad0bd7db3af
Ivan Nieto <ivanksy@icloud.com>
2016-07-24 00:49:27 -0500

    Sticky

---
+++ js/main.js
@@ -0,0 +1,41 @@
+;(function(){
+
+	let sticky = false
+
+	$(window).scroll(function(){
+		const inBottom = isInBottom()
+
+		if (inBottom && !sticky){
+			// Mostrar la navegación sticky
+			sticky = true
+			stickNavigation()
+		}
+		if(!inBottom && sticky){
+			// Ocultar la navegación sticky
+			sticky = false
+			unStickNavigation()
+
+		}
+	})
+
+	function stickNavigation(){
+		$("#description").addClass("fixed").removeClass("absolute")
+		$("#navigation").addClass("hidden")
+		$("#sticky-navigation").removeClass("hidden")
+	}
+
+	function unStickNavigation(){
+		$("#description").removeClass("fixed").addClass("absolute")
+		$("#navigation").removeClass("hidden")
+		$("#sticky-navigation").addClass("hidden")
+	}
+
+	function isInBottom(){
+		const $description = $("#description")
+		const descriptionHeight = $description.height()
+
+		return $(window).scrollTop() > $(window).height() - (descriptionHeight * 1.7)
+	}
+
+
+})()
